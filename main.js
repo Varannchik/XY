29eb0c815c700c81b1c4cf110e6b4926fb2504fa
Varannchik <varann@inbox.ru>
2019-06-06 23:43:54 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-//повторяет
+
 let el=document.querySelector('.sq');
 let dot=document.querySelector('.dot');
+let dot_z=document.querySelector('.dot_z');
 let x=0;
 let y=0;
 el.onmousemove = function(e) {
-    x=e.pageX;
-    y=e.pageY;      
-    dot.style.top=y+220+'px';
-    dot.style.left=x+'px'; 
+ 
+    if(e.target.closest('.sq')){
+        let target = e.target.closest('.sq'); 
+        let targetC = target.getBoundingClientRect();
+         x = e.clientX - targetC.left;
+         y = e.clientY - targetC.top;
+         
+         dot.style.top=y+20+'px';
+         dot.style.left=x+240+'px'; 
+         dot_z.style.top=-y+440+'px';
+         dot_z.style.left=x+20+'px'; 
+        
+      }
 }
 
-//зеркальное 
-let sq2=document.querySelector('.sq_z');
-let dot_z=document.querySelector('.dot_z');
-let x1=0;
-let y1=0;
-sq2.onmousemove = function(e) {
-    x1=e.pageX;
-    y1=e.pageY;      
-    dot_z.style.top=-y1+460+'px';
-    dot_z.style.left=x1+'px'; 
-}
+
+
 
 
 
